16ef9ea6c1cc560c57d5c1148a29e615fbf7a311
unknown <wcourraum248@ulacit.ed.cr>
2021-07-11 17:25:12 -0600

    Controllers Added ServicioCliente, Sucursal

---
+++ WebApiSegura/Controllers/ServicioClienteController.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSegura.Models;
+
+namespace WebApiSegura.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/ServicioCliente")]
+    public class ServicioClienteController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetId(int id)
+        {
+            ServicioCliente servicioCliente = new ServicioCliente();
+
+            try
+            {
+                using (SqlConnection sqlConnection = new
+                    SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT *
+                                                             FROM ServicioCliente
+                                                             WHERE CodigoServicio = @Codigo", sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@Codigo", id);
+                    sqlConnection.Open();
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        servicioCliente.CodigoServicio = sqlDataReader.GetInt32(0);
+                        servicioCliente.CodigoUsuario = sqlDataReader.GetInt32(1);
+                        servicioCliente.FechaCreacion = sqlDataReader.GetDateTime(2);
+                        servicioCliente.Descripcion = sqlDataReader.GetString(3);
+                        servicioCliente.TipoAyuda = sqlDataReader.GetString(4);
+
+                    }
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            return Ok(servicioCliente);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            List<ServicioCliente> servicios = new List<ServicioCliente>();
+            try
+            {
+                using (SqlConnection sqlConnection = new
+                    SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT *
+                                                             FROM ServicioCliente", sqlConnection);
+
+                    sqlConnection.Open();
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                    while (sqlDataReader.Read())
+                    {
+                        ServicioCliente servicioCliente = new ServicioCliente();
+                        servicioCliente.CodigoServicio = sqlDataReader.GetInt32(0);
+                        servicioCliente.CodigoUsuario = sqlDataReader.GetInt32(1);
+                        servicioCliente.FechaCreacion = sqlDataReader.GetDateTime(2);
+                        servicioCliente.Descripcion = sqlDataReader.GetString(3);
+                        servicioCliente.TipoAyuda = sqlDataReader.GetString(4);
+                        servicios.Add(servicioCliente);
+                    }
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            return Ok(servicios);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Ingresar(ServicioCliente servicioCliente)
+        {
+            if (servicioCliente == null)
+                return BadRequest();
+
+            try
+            {
+                using (SqlConnection sqlConnection = new
+                    SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO ServicioCliente (CodigoUsuario, FechaCreacion, Descripcion, TipoAyuda)
+                                                             VALUES (@CodigoUsuario, @FechaCreacion, @Descripcion, @TipoAyuda) ", sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@CodigoUsuario", servicioCliente.CodigoUsuario);
+                    sqlCommand.Parameters.AddWithValue("@FechaCreacion", servicioCliente.FechaCreacion);
+                    sqlCommand.Parameters.AddWithValue("@Descripcion", servicioCliente.Descripcion);
+                    sqlCommand.Parameters.AddWithValue("@TipoAyuda", servicioCliente.TipoAyuda);
+
+
+                    sqlConnection.Open();
+
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(servicioCliente);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Actualizar(ServicioCliente servicioCliente)
+        {
+            if (servicioCliente == null)
+                return BadRequest();
+
+            try
+            {
+                using (SqlConnection sqlConnection = new
+                    SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"UPDATE ServicioCliente SET Descripcion = @Descripcion,
+                                                                             CodigoUsuario = @CodigoUsuario,
+                                                                             FechaCreacion = @FechaCreacion,
+                                                                             TipoAyuda=@TipoAyuda
+                                                             WHERE CodigoServicio = @CodigoServicio ", sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@CodigoServicio", servicioCliente.CodigoServicio);
+                    sqlCommand.Parameters.AddWithValue("@Descripcion", servicioCliente.Descripcion);
+                    sqlCommand.Parameters.AddWithValue("@CodigoUsuario", servicioCliente.CodigoUsuario);
+                    sqlCommand.Parameters.AddWithValue("@FechaCreacion", servicioCliente.FechaCreacion); 
+                    sqlCommand.Parameters.AddWithValue("@TipoAyuda", servicioCliente.TipoAyuda);
+                    
+
+                    sqlConnection.Open();
+
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(servicioCliente);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Eliminar(int id)
+        {
+            if (id < 1)
+                return BadRequest();
+
+            try
+            {
+                using (SqlConnection sqlConnection = new
+                    SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"DELETE SERVICIOCLIENTE WHERE CodigoServicio = @Codigo ", sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@Codigo", id);
+
+                    sqlConnection.Open();
+
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(id);
+        }
+
+    }
+}
---
+++ WebApiSegura/Controllers/SucursalController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSegura.Models;
+
+namespace WebApiSegura.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Sucursal")]
+    public class SucursalController : ApiController
+    {
+            [HttpGet]
+            public IHttpActionResult GetId(int id)
+            {
+                Sucursal sucursal = new Sucursal();
+
+                try
+                {
+                    using (SqlConnection sqlConnection = new
+                        SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                    {
+                        SqlCommand sqlCommand = new SqlCommand(@"SELECT *
+                                                             FROM Sucursal
+                                                             WHERE Codigo = @Codigo", sqlConnection);
+
+                        sqlCommand.Parameters.AddWithValue("@Codigo", id);
+                        sqlConnection.Open();
+
+                        SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                        while (sqlDataReader.Read())
+                        {
+                            sucursal.Codigo = sqlDataReader.GetInt32(0);
+                            sucursal.Ubicacion = sqlDataReader.GetString(1);
+                            sucursal.Nombre = sqlDataReader.GetString(2);
+                            sucursal.Estado = sqlDataReader.GetString(3);
+                        }
+
+                        sqlConnection.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return InternalServerError(ex);
+                }
+                return Ok(sucursal);
+            }
+
+            [HttpGet]
+            public IHttpActionResult GetAll()
+            {
+                List<Sucursal> sucursales = new List<Sucursal>();
+                try
+                {
+                    using (SqlConnection sqlConnection = new
+                        SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                    {
+                        SqlCommand sqlCommand = new SqlCommand(@"SELECT *
+                                                             FROM Sucursal", sqlConnection);
+
+                        sqlConnection.Open();
+                        SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                        while (sqlDataReader.Read())
+                        {
+                            Sucursal sucursal = new Sucursal();
+
+                            sucursal.Codigo = sqlDataReader.GetInt32(0);
+                            sucursal.Ubicacion = sqlDataReader.GetString(1);
+                            sucursal.Nombre = sqlDataReader.GetString(2);
+                            sucursal.Estado = sqlDataReader.GetString(3);
+                            sucursales.Add(sucursal);
+                        }
+                        sqlConnection.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return InternalServerError(ex);
+                }
+                return Ok(sucursales);
+            }
+
+            [HttpPost]
+            public IHttpActionResult Ingresar(Sucursal sucursal)
+            {
+                if (sucursal == null)
+                    return BadRequest();
+
+                try
+                {
+                    using (SqlConnection sqlConnection = new
+                        SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                    {
+                        SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO Sucursal (Nombre, Ubicacion, Estado)
+                                                             VALUES (@Nombre,@Ubicacion, @Estado) ", sqlConnection);
+
+                        sqlCommand.Parameters.AddWithValue("@Nombre", sucursal.Nombre);
+                        sqlCommand.Parameters.AddWithValue("@Ubicacion", sucursal.Ubicacion);
+                        sqlCommand.Parameters.AddWithValue("@Estado", sucursal.Estado);
+
+                        sqlConnection.Open();
+
+                        int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                        sqlConnection.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return InternalServerError(ex);
+                }
+
+                return Ok(sucursal);
+            }
+
+            [HttpPut]
+            public IHttpActionResult Actualizar(Sucursal sucursal)
+            {
+                if (sucursal == null)
+                    return BadRequest();
+
+                try
+                {
+                    using (SqlConnection sqlConnection = new
+                        SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                    {
+                        SqlCommand sqlCommand = new SqlCommand(@"UPDATE Sucursal SET Nombre = @Nombre,
+                                                                                Ubicacion = @Ubicacion,
+                                                                             Estado = @Estado
+                                                             WHERE Codigo = @Codigo ", sqlConnection);
+
+                        sqlCommand.Parameters.AddWithValue("@Codigo", sucursal.Codigo);
+                        sqlCommand.Parameters.AddWithValue("@Nombre", sucursal.Nombre);
+                        sqlCommand.Parameters.AddWithValue("@Ubicacion", sucursal.Ubicacion);
+                        sqlCommand.Parameters.AddWithValue("@Estado", sucursal.Estado);
+
+                        sqlConnection.Open();
+
+                        int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                        sqlConnection.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return InternalServerError(ex);
+                }
+
+                return Ok(sucursal);
+            }
+
+            [HttpDelete]
+            public IHttpActionResult Eliminar(int id)
+            {
+                if (id < 1)
+                    return BadRequest();
+
+                try
+                {
+                    using (SqlConnection sqlConnection = new
+                        SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                    {
+                        SqlCommand sqlCommand = new SqlCommand(@"DELETE Sucursal WHERE Codigo = @Codigo", sqlConnection);
+
+                        sqlCommand.Parameters.AddWithValue("@Codigo", id);
+
+                        sqlConnection.Open();
+
+                        int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                        sqlConnection.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return InternalServerError(ex);
+                }
+
+                return Ok(id);
+            }
+        }
+    }
+
